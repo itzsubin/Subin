c46cd1f631d475eb6e18120795d496389ce3fd63
itzsubin <subinghimire696@gmail.com>
2026-01-25 22:54:59 -0600

    Made changes in project and fix the deployement error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
+        "./public/index.html"
     ],
-    darkMode: 'media', // or 'class'
     theme: {
         extend: {},
     },
