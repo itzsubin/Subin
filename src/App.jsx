3b2fa8afff158a1168f611fd1bf30e2a2637cf19
itzsubin <subinghimire696@gmail.com>
2026-02-02 00:05:09 -0600

    Physics added in the cursor moment animation that matches the cosmo theme

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,24 @@ import Projects from './Sections/Projects';
 import Contact from './Sections/Contact';
 import FloatingParticles from './Common/Stars/FloatingParticles';
 import { useTheme } from './Common/Theme/ThemeContext';
-
 function App() {
   const { theme } = useTheme();
 
   return (
-    <>
-      {theme === 'dark' && <FloatingParticles />}
-      <div style={{ zIndex: 1, position: 'relative' }}>
-        <Navbar />
-        <Media />
-        <Subu />
-        <Skills />
-        <Projects />
-        <Contact />
-        <footer >
-          © Subin
-        </footer>
-      </div>
-    </>
-
+      <>
+        {theme === 'dark' && <FloatingParticles />}
+        <div style={{ zIndex: 1, position: 'relative' }}>
+          <Navbar />
+          <Media />
+          <Subu />
+          <Skills />
+          <Projects />
+          <Contact />
+          <footer >
+            © Subin
+          </footer>
+        </div>
+      </>
   );
 }
 
